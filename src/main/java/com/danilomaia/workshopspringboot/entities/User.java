d0be0d9623ceb21345d8a84dc0956c5379cc3b2f
Danilo Maia <isaqquemaiia7@outlook.com.br>
2023-03-02 18:47:06 -0300

    H2 database, test profile, JPA

--- src/main/java/com/danilomaia/workshopspringboot/entities/User.java
+++ src/main/java/com/danilomaia/workshopspringboot/entities/User.java
@@ -1,11 +1,20 @@
 package com.danilomaia.workshopspringboot.entities;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+	@Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
     private String name;
     private String email;
