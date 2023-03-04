76fa66f63386324b6014e87c06eb37bc5b478657
Danilo Maia <isaqquemaiia7@outlook.com.br>
2023-03-03 21:33:28 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/danilomaia/workshopspringboot/config/TestConfig.java
+++ src/main/java/com/danilomaia/workshopspringboot/config/TestConfig.java
@@ -24,10 +24,12 @@ public class TestConfig implements CommandLineRunner {
     private final CategoryRepository categoryRepository;
     private final ProductRepository productRepository;
 
-    public TestConfig(UserRepository userRepository,
-                      OrderRepository orderRepository,
-                      CategoryRepository categoryRepository,
-                      ProductRepository productRepository) {
+    public TestConfig(
+            UserRepository userRepository,
+            OrderRepository orderRepository,
+            CategoryRepository categoryRepository,
+            ProductRepository productRepository
+    ) {
         this.userRepository = userRepository;
         this.orderRepository = orderRepository;
         this.categoryRepository = categoryRepository;
@@ -46,6 +48,20 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -53,8 +69,7 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
