44946848bc3420f690b21ed5f77a0a98f2c1dd8d
Danilo Maia <isaqquemaiia7@outlook.com.br>
2023-03-07 22:07:09 -0300

    Exception handling - update

--- src/main/java/com/danilomaia/workshopspringboot/services/UserService.java
+++ src/main/java/com/danilomaia/workshopspringboot/services/UserService.java
@@ -4,6 +4,7 @@ import com.danilomaia.workshopspringboot.entities.User;
 import com.danilomaia.workshopspringboot.repositories.UserRepository;
 import com.danilomaia.workshopspringboot.services.exceptions.DatabaseException;
 import com.danilomaia.workshopspringboot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
